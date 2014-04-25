78352754e09057f118591601b6dd91dbbeee3630
James Hollingworth <jamiehollingworth@gmail.com>
2014-04-26 00:26:38 +0100

    Showing properly formatted results

--- client/journey/journeyController.js
+++ client/journey/journeyController.js
@@ -1,4 +1,16 @@
+var moment = require('moment');
+
 function JourneyController($scope) {
+    $scope.timeArrives = timeArrives();
+    $scope.minToDeparture = minToDeparture();
+
+    function timeArrives() {
+        return moment($scope.journey.arrival).format('H:mm')
+    }
+
+    function minToDeparture() {
+        return moment($scope.journey.start).fromNow()
+    }
 }
 
 JourneyController.$inject = ['$scope'];
